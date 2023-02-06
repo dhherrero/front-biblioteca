af080c57bd2367aaa1c1deeda8e42f472ccf9c3a
Diego Herrero <d.herrero8@usp.ceu.es>
2023-02-06 20:44:52 +0100

    Fix last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { useState } from 'react'
+import {Link} from "wouter";
 import './App.css'
 
 import Routes from './routes';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <div>
+        <Link to="/" target="_blank">
+        <img src="/grema.png" className="logo" alt="GREMA logo" />
+        </Link>
+        <h1>Biblioteca GREMA</h1>
+      </div>
       <Routes></Routes>
       
       
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- src/page/home/home.jsx
+++ src/page/home/home.jsx
@@ -3,7 +3,6 @@ import './home.css'
 export default function Home (){
     return (
         <div>
-            <Title></Title>
             <section className='App-content'>
                 <Link to='/login' className='sign'> Sign in</Link>
                 <Link to='/login' className='sign'> Sign up</Link>
