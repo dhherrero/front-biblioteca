13ba49658acc8923a1a3371bd6f4911d80d4d614
Diego Herrero <d.herrero8@usp.ceu.es>
2023-05-23 20:03:54 +0200

    Margin top

--- src/components/AllUsers/AllUsers.jsx
+++ src/components/AllUsers/AllUsers.jsx
@@ -3,6 +3,7 @@ import Navbar from "../NavBar/Navbar"
 import User from "../User/User"
 import { useEffect, useState } from "react";
 import {getAllUsers} from "../../service/userService";
+import "./AllUsers.css"
 
 export default function AllUsers (){
     const [usuarios, setUsuarios] = useState([])
@@ -15,7 +16,7 @@ export default function AllUsers (){
     return(
         <>
             <Navbar />
-            <h2 className="tt2"> USUARIOS</h2>
+            <h2 className="tt5"> USUARIOS</h2>
             <div className="listaUsuarios">
             {usuarios.map((usuario,i)=>{
                 return(
